77796b167674c52391bc678f003653241b9d2a07
Himanshu Sheetlani <himanshu.sheetlani2909@gmail.com>
2026-06-24 23:12:02 +0530

    added attempt and question models

---
+++ server/model/attempt.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Model, Schema } from "mongoose";
+
+export interface AttemptI{
+    userId: string,
+    questionId: string,
+    time: number,
+    hint: boolean,
+}
+
+const attemptSchema: Schema<AttemptI> = new mongoose.Schema({
+    userId:{
+        type: String,
+        required: true,
+    },
+    questionId:{
+        type: String,
+        required: true,
+    },
+    time: {
+        type: Number,
+        required: true,
+    },
+    hint:{
+        type: Boolean,
+        required: true,
+    },
+})
+
+export const Attempt: Model<AttemptI> = mongoose.model("Attempt", attemptSchema)
---
+++ server/model/question.model.ts
@@ -0,0 +1,34 @@
+import mongoose, { Model, Schema } from "mongoose";
+
+export interface questionI{
+    userId: string,
+    questionNo: number,
+    name: string,
+    tag: "easy" | "medium" | "hard",
+    link: string,
+}
+
+const questionSchema: Schema<questionI> = new mongoose.Schema({
+    userId:{
+        type: String,
+        required: true,
+    },
+    questionNo:{
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    name:{
+        type: String,
+    },
+    tag:{
+        type: String,
+        required: true,
+    },
+    link:{
+        type: String,
+        required: true,
+    },
+})
+
+export const Quesion: Model<questionI> = mongoose.model("Questions", questionSchema)
