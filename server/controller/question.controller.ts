f96e39384479a1529c754fc4dcf16208161e129f
Himanshu Sheetlani <himanshu.sheetlani2909@gmail.com>
2026-06-25 17:37:32 +0530

    feat: implement getQuestions functionality to retrieve questions by user ID

--- server/controller/question.controller.ts
+++ server/controller/question.controller.ts
@@ -1,5 +1,8 @@
+import { Question, type questionI } from "../model/question.model.ts";
+import jwt from "jsonwebtoken";
 
-import { Quesion, type questionI } from "../model/question.model.ts";
+import type { userToken } from "./checkAuth.controller.ts";
+import type { Request, Response } from "express";
 
 interface data{
     userId: string,
@@ -9,8 +12,30 @@ interface data{
     link: string
 }
 
+
 export const createQuestions = async (body: data) =>{
-    const question = await Quesion.create(body)
+    const question = await Question.create(body)
     const questionId = question._id
     return questionId
+}
+
+export const getQuestions = async(req: Request, res: Response) =>{
+    const token = req.cookies.token
+    const secret = process.env.JWT_SECRET as string
+
+    if (!token){
+        return res.status(401).json({msg: "Unauthorized User"})
+    }
+
+    try{
+        const user = jwt.verify(token, secret) as userToken
+        const userId = user.id
+
+        const questions: questionI[] = await Question.find({userId})
+
+        return res.status(200).json({msg: "Data Retrieved Successfully", questions})
+    }
+    catch(e){
+        return res.status(400).json("Somthing Went Wrong")
+    }
 }
--- server/routes/dashboard.route.ts
+++ server/routes/dashboard.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createAttempt, getAttempt } from "../controller/attempt.controller.ts";
+import { getQuestions } from "../controller/question.controller.ts";
 
 const route = Router();
 
 route.post('/attempt', createAttempt)
 route.get('/question/:id', getAttempt)
+route.get("/question", getQuestions)
 
 export default route
