6b4b51b189da26f8ef1d725d97996231fc38ac76
Himanshu Sheetlani <himanshu.sheetlani2909@gmail.com>
2026-06-25 19:31:58 +0530

    feat: add getStats endpoint to retrieve user statistics including difficulty tags, average time taken, and recent attempts

---
+++ server/controller/stats.controller.ts
@@ -0,0 +1,88 @@
+import type { Request, Response } from "express";
+import { Question, type questionI } from "../model/question.model.ts";
+import { Attempt } from "../model/attempt.model.ts";
+import { error } from "node:console";
+
+
+export const getStats = async (req: Request, res: Response) => {
+    const user = req.user
+    const userId = user.id
+
+    const difficultyTag = async ()=>{
+        const tag = {
+            easy: 0,
+            medium: 0,
+            hard: 0,
+        }
+        const difficultyStats = await Question.aggregate([
+            {
+                $match:{
+                    userId: userId
+                }
+            },
+            {
+                $group:{
+                    _id: "$tag",
+                    count: {
+                        $sum: 1
+                    }
+                }
+            }
+        ])
+
+        difficultyStats.forEach(item => {
+            tag[item._id as "easy" | "medium" | "hard"] = item.count;
+        });
+        return tag
+    }
+
+    const avgTimeTaken = async() =>{
+        try{
+            const time = await Attempt.aggregate([
+                {
+                    $match: {
+                        userId: userId
+                    }
+                },
+                {
+                    $group:{
+                        _id: null,
+                        avgTime: {
+                            $avg: '$time'
+                        }
+                    }
+                }
+            ])
+            return time[0].avgTime
+        }
+        catch(e){
+            return (e)
+        }
+    }
+
+    const recent = async() =>{
+        try{
+            const recentQuestions = await Attempt.find({
+                userId
+            })
+            .sort({updatedAt: -1})
+            .limit(5)
+            return recentQuestions
+        }
+        catch(e){
+            return e
+        }
+    }
+
+    try{
+        const tag = await difficultyTag()
+        const totalQuestion = tag.easy + tag.medium + tag.hard
+        const avgTime = await avgTimeTaken()
+        const recentAttempt = await recent()
+        
+        res.status(200).json({msg: "Successful", data: {tag, totalQuestion, avgTime, recentAttempt}})
+    }
+    catch(e){
+        res.status(400).json({msg: "Somthing went Wrong"})
+    }
+}
--- server/routes/dashboard.route.ts
+++ server/routes/dashboard.route.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { createAttempt, getAttempt } from "../controller/attempt.controller.ts";
 import { getQuestions } from "../controller/question.controller.ts";
 import {authMiddleware} from '../middleware/checkAuth.middleware.ts'
+import { getStats } from "../controller/stats.controller.ts";
 
 const route = Router();
 
 route.post('/attempt', authMiddleware , createAttempt)
 route.get('/question/:id', authMiddleware , getAttempt)
 route.get('/question', authMiddleware , getQuestions)
+route.get('/stats', authMiddleware, getStats)
 
 export default route
