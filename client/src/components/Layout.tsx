7862e268231314af75a0cbbfa7dacb9239915e53
Himanshu Sheetlani <himanshu.sheetlani2909@gmail.com>
2026-07-04 22:58:42 +0530

    fix: make fetchData prop optional and enhance layout styling for better responsiveness

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -3,13 +3,13 @@ import Navbar from "./Navbar";
 import CreateAttempt from "./CreateAttempt";
 
 type LayoutProps = {
-  fetchData: () => void | Promise<void>;
+  fetchData?: () => void | Promise<void>;
 };
 
 const Layout: React.FC<LayoutProps> = ({ fetchData }) => {
   const [popup, setPopup] = useState(false);
   return (
-    <div className="flex justify-center text-white absolute">
+    <div className="flex justify-center text-white absolute w-full left-0 right-0 z-50">
       <Navbar setPopup={setPopup} />
       <CreateAttempt popup={popup} setPopup={setPopup} fetchData={fetchData}/>
     </div>
