9b6c282988ccdaf40c959409ae6804d2c39226e9
Himanshu Sheetlani <himanshu.sheetlani2909@gmail.com>
2026-07-11 19:50:11 +0530

    fix (Bug #16): improve error handling and validation in login and signup methods

--- server/controller/auth.controller.ts
+++ server/controller/auth.controller.ts
@@ -1,93 +1,107 @@
-import type { Response, Request } from "express"
-import jwt from "jsonwebtoken"
+import type { Response, Request } from "express";
+import jwt from "jsonwebtoken";
 import { User } from "../model/user.model.ts";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
 import type { UserI } from "../model/user.model.ts";
 
-const JWT_Secret = process.env.JWT_SECRET as string
+const JWT_Secret = process.env.JWT_SECRET as string;
 
-export const login = async (req: Request, res: Response)=>{
-    try{
-        const {username, password} = req.body;
-        if (!username || !password ){
-            return res.status(401).json({"msg": "invalid data entered"})
-        }
-        const user = await User.findOne({username})
-
-        if (!user) {
-            return res.status(401).json({"msg":"user not found"})
-        }
-
-        const pass: boolean = await bcrypt.compare(password, user.password)
-        if (!pass){
-            return res.status(401).json({"msg":"invalid password"})
-        }
-
-        const token = jwt.sign({
-                id: user._id.toString(), 
-                email: user.email, 
-                username: user.username
-            }, 
-            JWT_Secret, 
-            {
-                expiresIn: '7d'
-            } 
-        )
-        
-        res.cookie('token', token, {httpOnly: true, maxAge: 1000*60*60*24*7})
-        return res.status(200).json({
-            "msg": "login Successfull",
-            "username": user.username,
-        })
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(401).json({ msg: "invalid data entered" });
     }
-    catch(e){
-        return res.status(400).json({msg: e})
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ msg: "user not found" });
     }
 
-}
-
-export const signup = async (req: Request, res: Response) =>{
-    try{
-        const {username, email, password} = req.body;
-        if (!username || !email || !password ){
-            return res.status(422).json({"msg": "invalid data entered"})
-        }
-        const checkUser = await User.findOne({$or: [{ username }, { email }]})
-
-        if (checkUser) {
-            return res.status(409).json({"msg":"user already exist"})
-        }
-        const salt = await bcrypt.genSalt(12)
-        const hashPassword = await bcrypt.hash(password, salt)
-        const user = await User.create({username, email, password: hashPassword})
-
-        const token = jwt.sign({
-                id: user._id.toString(), 
-                email: user.email, 
-                username: user.username
-            }, 
-            JWT_Secret, 
-            {
-                expiresIn: '7d'
-            } 
-        )
-
-        res.cookie('token', token, {httpOnly: true, maxAge: 1000*60*60*24*7})
-
-        return res.status(201).json({
-            "msg": "user created successfully",
-            "username": user.username,
-        })
-    } 
-    catch(e){
-        return res.status(400).json({"msg": e})
+    const pass: boolean = await bcrypt.compare(password, user.password);
+    if (!pass) {
+      return res.status(401).json({ msg: "invalid password" });
     }
-}
 
-export const logout = (req: Request, res: Response) =>{
-    res.clearCookie('token', {
-        httpOnly: true,
-    })
-    return res.status(200).json({msg: "Logged out successfully"})
-}
+    const token = jwt.sign(
+      {
+        id: user._id.toString(),
+        email: user.email,
+        username: user.username,
+      },
+      JWT_Secret,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+    return res.status(200).json({
+      msg: "login Successfull",
+      username: user.username,
+    });
+  } catch (e) {
+    return res.status(400).json({ msg: e });
+  }
+};
+
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(422).json({ msg: "invalid data entered" });
+    }
+    const usernameRegex = /^[a-zA-Z][a-zA-Z0-9_]{2,19}$/;
+    if (!usernameRegex.test(username)) {
+      return res.status(422).json({ msg: "Username can Only Contain Letters, Number and _" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(422).json({ msg: "Invalid Email Address" });
+    }
+    const checkUser = await User.findOne({ $or: [{ username }, { email }] });
+
+    if (checkUser) {
+      return res.status(409).json({ msg: "user already exist" });
+    }
+    const salt = await bcrypt.genSalt(12);
+    const hashPassword = await bcrypt.hash(password, salt);
+    const user = await User.create({ username, email, password: hashPassword });
+
+    const token = jwt.sign(
+      {
+        id: user._id.toString(),
+        email: user.email,
+        username: user.username,
+      },
+      JWT_Secret,
+      {
+        expiresIn: "7d",
+      },
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    return res.status(201).json({
+      msg: "user created successfully",
+      username: user.username,
+    });
+  } catch (e) {
+    return res.status(400).json({ msg: e });
+  }
+};
+
+export const logout = (req: Request, res: Response) => {
+  res.clearCookie("token", {
+    httpOnly: true,
+  });
+  return res.status(200).json({ msg: "Logged out successfully" });
+};
